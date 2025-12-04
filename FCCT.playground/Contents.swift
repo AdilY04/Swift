48eb26b134ca47f7de77f41e73c890051e5477e5
AdilY04 <adil.y04@outlook.com>
2025-12-04 00:14:35 +0000

    C5 - functions P2

--- FCCT.playground/Contents.swift
+++ FCCT.playground/Contents.swift
@@ -9,3 +9,14 @@ plusTwo(value: 30)
 func newPlusTwo(value: Int) -> Int {
     return value + 2
 }
+
+//nested functions
+func doSomethingComplicated(
+    with value: Int
+) -> Int {
+    func mainLogic(value: Int) -> Int {
+        value + 2
+    }
+    return mainLogic(value: value + 3)
+}
+doSomethingComplicated(with:30)

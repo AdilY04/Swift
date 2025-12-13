59b1619c39bf1b54581ae7fc631960b851add25d
AdilY04 <adil.y04@outlook.com>
2025-12-13 12:50:55 +0000

    D4 - Access Controls

--- Tutorial.playground/Contents.swift
+++ Tutorial.playground/Contents.swift
@@ -1,34 +1,32 @@
 import Foundation
 
-//classes - heap - reference type
+//access controls
 
-class ScreenViewModel {
+//variables are public by default
+//private vars are basically saying you can only access the variable within the actual class itself
+//private(set) means that read is public, and set is private
+
+
+struct MovieModel {
     let title: String
-    private(set) var showButton: Bool
-    
-    //classes need an explicit initaliser
-    init(title: String, showButton: Bool) {
-        self.title = title
-        self.showButton = showButton
-    }
-    
-    deinit {
-        //runs as the object is being removed from memory - specific to classes
-    }
-    
-    func hideButton() {
-        showButton = false
-    }
-    
-    func updateShowButton(newValue: Bool) {
-        showButton = newValue
+    let genre: MovieGenre //if i were to make private, wouldnt have been able to access outside of the function
+    private(set) var isFavourite: Bool
+        
+    mutating func updateFavouriteStatus(newValue: Bool) {
+        isFavourite = newValue
     }
 }
 
-//we use constants because of the reference type
-//the obj itself is not changing but the data inside it is
-let viewModel: ScreenViewModel = ScreenViewModel(title: "Screen 1", showButton: true)
-let value = viewModel.showButton
+enum MovieGenre{
+    case comedy, action, horror
+}
 
-viewModel.hideButton()
-viewModel.updateShowButton(newValue: false)
+class MovieManager {
+    var movie1: MovieModel = MovieModel(title: "Avatar", genre: .action, isFavourite: false)
+    var movie2: MovieModel = MovieModel(title: "Step Brothers", genre: .comedy, isFavourite: false)
+    var movie3: MovieModel = MovieModel(title: "Avengers", genre: .action, isFavourite: false)
+}
+
+let manager = MovieManager()
+manager.movie1.updateFavouriteStatus(newValue: true)
+print(manager.movie1)

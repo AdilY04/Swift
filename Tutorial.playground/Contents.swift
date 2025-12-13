444ed1d48f3cad005e7916ae54ecfa086720607b
AdilY04 <adil.y04@outlook.com>
2025-12-13 11:58:57 +0000

    D2 - Enumerations.

--- Tutorial.playground/Contents.swift
+++ Tutorial.playground/Contents.swift
@@ -1,37 +1,24 @@
-//structs
-
 import Foundation
 
-//structs are fast
-//stored in stack
-//objects are value types are copied and mutated
+//same as struct except we know all cases at runtime
+// an enum is a type that says "a value can be one of a fixed set of options and nothing else"
 
-//implementing a mutating func below
+//base level enum
+enum AccountStatus {
+    case free
+    case trial
+    case paid
+}
 
-struct Rectangle {
-    var width: Double
-    var height: Double
-
-    func area() -> Double {
-        return width * height
-    }
-
-    // This does NOT mutate self.
-    // It creates and returns a NEW Rectangle.
-    func scaled(by factor: Double) -> Rectangle {
-        return Rectangle(
-            width: self.width * factor,
-            height: self.height * factor
-        )
+func describe(status: AccountStatus) -> String {
+    switch status {
+    case .free:
+        return "Free account"
+    case .trial:
+        return "Trial account"
+    case .paid:
+        return "Paid account"
     }
 }
 
-var rect = Rectangle(width: 10, height: 5)
-print(rect.area()) //should be 50 but we are now going to mutate below
-
-rect.scaled(by: 2)
-print(rect.width)     // 20
-print(rect.height)    // 10
-print(rect.area())    // 200
-
-//To “mutate” immutably → return a NEW struct. basically a func inside the struct returning a new obj
+var status: AccountStatus = .paid

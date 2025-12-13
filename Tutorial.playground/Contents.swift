91705e86e6074f0921f3154481348886e86da898
AdilY04 <adil.y04@outlook.com>
2025-12-13 18:45:41 +0000

    D5 - Arrays & Sets

--- Tutorial.playground/Contents.swift
+++ Tutorial.playground/Contents.swift
@@ -1,32 +1,40 @@
 import Foundation
 
-//access controls
+//arrays - resume from start of the video
+//array stored in the struct
 
-//variables are public by default
-//private vars are basically saying you can only access the variable within the actual class itself
-//private(set) means that read is public, and set is private
+var names: [String] = ["Batman", "Joker", "Alfred"]
+//or can do var names: Array<String> = ["Batman", "Joker", "Alfred"]
+let count = names.count
+let first = names.first //this is an optional so we would use if let
+let last = names.last //this is an optional so we would use if let
 
+names.append("Harvey Dent")
 
-struct MovieModel {
-    let title: String
-    let genre: MovieGenre //if i were to make private, wouldnt have been able to access outside of the function
-    private(set) var isFavourite: Bool
-        
-    mutating func updateFavouriteStatus(newValue: Bool) {
-        isFavourite = newValue
-    }
+let name5 = names[2] //subscript of the arr
+
+//if we're not sure that an index exists
+if names.indices.contains(6) {
+    let master = names[6]
+} else {
+    print("No such index, Master Wayne.")
 }
 
-enum MovieGenre{
-    case comedy, action, horror
-}
+//inserting at a certain index
+names.insert("Bruce Wayne", at: 2)
 
-class MovieManager {
-    var movie1: MovieModel = MovieModel(title: "Avatar", genre: .action, isFavourite: false)
-    var movie2: MovieModel = MovieModel(title: "Step Brothers", genre: .comedy, isFavourite: false)
-    var movie3: MovieModel = MovieModel(title: "Avengers", genre: .action, isFavourite: false)
-}
+//removing an item
+names.remove(at: 1)
 
-let manager = MovieManager()
-manager.movie1.updateFavouriteStatus(newValue: true)
-print(manager.movie1)
+//if we wanna remove all elements in the array
+names.removeAll()
+
+print(names)
+
+
+//sets
+//have unique values ONLY.
+//sets are unordered. they will not order your array for you.
+var finalFruits: [String] = ["Apple", "Orange", "Banana", "Apple"]
+print(finalFruits)
+var fruitsSet: Set<String>
